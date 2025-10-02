22448b2be54a64dcc7e9b1326a1112e7c48d2ae3
Nayan-Mudewar <nayanmudewar2002@gmail.com>
2025-10-02 18:48:32 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/github/com/Nayan_Mudewar/Online/Quiz/App/OnlineQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.Nayan_Mudewar.Online.Quiz.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/com/Nayan_Mudewar/Online/Quiz/App/OnlineQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.Nayan_Mudewar.Online.Quiz.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
